ae5f80604e1a5903fe7f018cf19d2383b629edf0
JonasDark <s.william.luttge@gmail.com>
2022-02-11 12:11:30 -0500

    Lesson 2 HW

---
+++ HW2_Locators.py
@@ -0,0 +1,19 @@
+'''Automation Lesson 2 Homework: Locators:  Sean Luttge
+
+1. Amazon logo = By.XPATH, "//i[@class = 'a-icon a-icon-logo']"
+
+2. Continue button = By.ID, 'continue'
+
+3. Need help link = By.XPATH, "//span[@class= 'a-expander-prompt']"
+
+4. Forgot your password link = By.ID, 'auth-fpp-link-bottom'
+
+5. Other issues with Sign-In link = By.ID, 'ap-other-signin-issues-link'
+
+6. Create your Amazon account button = By.ID, 'createAccountSubmit'
+
+7. *Conditions of use link = By.XPATH, "//a[contains(@href, 'nodeId=508088')]"
+
+8. *Privacy Notice link = By.XPATH, "//a[contains(@href, 'nodeId=468496')]"
+'''
+
